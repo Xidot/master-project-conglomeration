0f052704ba12577e7c08ee8c00d89cbca4509a9a
sproid <cercel147@gmail.com>
2025-08-13 15:53:54 +0300

    update remove keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,15 @@
 
 # Setup the environment for the scripts and tools to be in path
 
-set -e
+# set -e
 
 THIS=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
+# TODO: FILL THESE IN
+export LLM_KEY="${LLM_KEY:-`cat llm_key`}" 
+export LLM_BASEURL="${LLM_BASEURL:-https://api.deepseek.com}"
+
+# Other requirements
 export PATH="$PATH:$THIS/scripts"
 source $THIS/venv/bin/activate
 
