66d1597d254e10272b4c237da1b8ac4aa743e3de
sproid <cercel147@gmail.com>
2025-08-16 12:22:11 +0300

    comments

--- scripts/test-cindex.sh
+++ scripts/test-cindex.sh
@@ -6,6 +6,7 @@ FUZZING_DIR=$(realpath $THIS/../experiments/fuzzing)
 pushd $FUZZING_DIR;
 
 # This is a dry compilation run
-./analyze.sh
+# Note: It's dry only if gen/prepare does not exist.
+./analyze.sh 
 
 popd;
